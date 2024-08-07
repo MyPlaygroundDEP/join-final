9b3d4dd6b560f7df4f94edbb7c8c0c602520e3ca
prajeethWolf <prajeeth31@gmail.com>
2024-08-07 19:08:37 +0530

    add customer records

--- db-script.sql
+++ db-script.sql
@@ -1,6 +1,11 @@
-CREATE TABLE customer
-(
-    id   VARCHAR(10) PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+NSERT INTO customer (id, name) VALUES ('C001', 'Kasun Sampath');
+INSERT INTO customer (id, name) VALUES ('C002', 'Nuwan Praddep');
+INSERT INTO customer (id, name) VALUES ('C003', 'Ruwan Ramidu');
+INSERT INTO customer (id, name) VALUES ('C004', 'Pasan Chamara');
+INSERT INTO customer (id, name) VALUES ('C005', 'Buddika Silva');
+INSERT INTO customer (id, name) VALUES ('C006', 'Chaminda Vass');
+INSERT INTO customer (id, name) VALUES ('C007', 'Bathiya Santhush');
+INSERT INTO customer (id, name) VALUES ('C008', 'Chamara Silva');
+INSERT INTO customer (id, name) VALUES ('C009', 'Ajantha Mendis');
+INSERT INTO customer (id, name) VALUES ('C010', 'Sura Boy');
 
-);
