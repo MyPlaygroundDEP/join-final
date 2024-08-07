c016f2ddb7ef18afdc4ee66b3210de1620b17621
ayeshmantha <ravindu.ayesh123@gmail.com>
2024-08-07 19:03:06 +0530

    add user records

---
+++ db-script.sql
@@ -0,0 +1,5 @@
+INSERT INTO "user"(username, name, password) VALUES ('amal123','Amal Perera','amal123');
+INSERT INTO "user"(username, name, password) VALUES ('kamal123','Kamal Silva','kamal123');
+INSERT INTO "user"(username, name, password) VALUES ('nimal123','Nimal fernando','nimal123');
+INSERT INTO "user"(username, name, password) VALUES ('sadun123','Sadun Shanaka','sadun123''');
+INSERT INTO "user"(username, name, password) VALUES ('kamidu123','Kamidu Mendis','kamidu123');
