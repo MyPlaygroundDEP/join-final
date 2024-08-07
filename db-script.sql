b51efa2eb308346cbe643d0ddc3a6c17050de2b5
ayeshmantha <ravindu.ayesh123@gmail.com>
2024-08-07 18:54:27 +0530

    table relation created

---
+++ db-script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE item(
+                     bar_code VARCHAR(50) PRIMARY KEY ,
+                     description VARCHAR(150) NOT NULL ,
+                     stock INT NOT NULL ,
+                     price DECIMAL
+);
