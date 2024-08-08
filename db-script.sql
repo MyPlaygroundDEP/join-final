115d45ed93b8f83e8149f716d22ce86b25c85a37
prajeethWolf <prajeeth31@gmail.com>
2024-08-08 11:01:20 +0530

    add order relation

--- db-script.sql
+++ db-script.sql
@@ -1,11 +1,9 @@
-NSERT INTO customer (id, name) VALUES ('C001', 'Kasun Sampath');
-INSERT INTO customer (id, name) VALUES ('C002', 'Nuwan Praddep');
-INSERT INTO customer (id, name) VALUES ('C003', 'Ruwan Ramidu');
-INSERT INTO customer (id, name) VALUES ('C004', 'Pasan Chamara');
-INSERT INTO customer (id, name) VALUES ('C005', 'Buddika Silva');
-INSERT INTO customer (id, name) VALUES ('C006', 'Chaminda Vass');
-INSERT INTO customer (id, name) VALUES ('C007', 'Bathiya Santhush');
-INSERT INTO customer (id, name) VALUES ('C008', 'Chamara Silva');
-INSERT INTO customer (id, name) VALUES ('C009', 'Ajantha Mendis');
-INSERT INTO customer (id, name) VALUES ('C010', 'Sura Boy');
-
+CREATE TABLE "order"
+(
+    id VARCHAR(10) PRIMARY KEY ,
+    date DATE,
+    customer_id VARCHAR(10) NOT NULL,
+    username VARCHAR(100) NOT NULL ,
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer,
+    CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES "user"
+);
